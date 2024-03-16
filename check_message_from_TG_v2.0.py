09e283a778d94d0aa7da23610bc7c82ffa8b193c
andy <andrei_miroshnikk@mail.ru>
2024-03-16 15:19:58 +0700

    #4

--- check_message_from_TG_v2.0.py
+++ check_message_from_TG_v2.0.py
@@ -1,10 +1,9 @@
 import time
-
 from telethon import TelegramClient, events
 import csv
 import re
 import ast
-
+from datetime import datetime
 
 api_id = '23796739'
 api_hash = 'b04f272bbefc7b1c6249bdcb3fd8f792'
@@ -28,13 +27,13 @@ while True:
                                 res_str = row[0].replace('(', '')
                                 res_str = res_str.replace('[', '')
                                 my_dict = ast.literal_eval(res_str)
-                                srt_file.append(my_dict['message.id'])
+                                srt_file.append(my_dict['message.id']) #разобраться добавить описание
                     with open(f'messages_from_{channel}.csv', 'a', newline='', encoding="utf-8") as csvfile:
                         messagewriter = csv.writer(csvfile, delimiter='&', quotechar='|', quoting=csv.QUOTE_MINIMAL)
                         print('save_to_', channel)
                         async for message in client.iter_messages(tg_channel[channel], limit=20):
                             if message.id not in srt_file:
-                                coin_find = re.findall('#(\w+)\s', message.text or "")
+                                coin_find = re.findall('#(\w+)\s', message.text or "") #  изменить на none
                                 if type(coin_find) == list:
                                     if coin_find != []:
                                         for find_coin in coin_find:
@@ -47,13 +46,20 @@ while True:
                                         coin = None
                                 else:
                                     coin = coin_find.group(1) if coin_find else None
-                                if message.text != None:
-                                    posSide = "short" if "short" in message.text or "шорт" in message.text or "Short" in message.text else "long" if "лонг" in message.text or "long" in message.text or "Long" in message.text else None
+                                if message.text != None: #поднять выше пере дпроверкой токена, зачем он так делко в коде.
+                                    #posSide = "short" if "short" in message.text or "шорт" in message.text or "Short" in message.text else "long" if "лонг" in message.text or "long" in message.text or "Long" in message.text else None
+                                    message_text_lower = (message.text).lower()
+                                    if "short" in message_text_lower or "шорт" in message_text_lower:
+                                        posSide = "short"
+                                    elif "long" in message_text_lower or "лонг" in message_text_lower:
+                                        posSide = "long"
+                                    else:
+                                        posSide = None
                                     input_find = re.search(r'твх.+-.+', message.text or "")
                                     input_point = re.findall(r"\d+.\d+|\d+", input_find[0]) if input_find else None
-                                    input_point = str(input_point).replace(',', '*')
+                                    input_point = str(input_point).replace(',', '*') #заменить на json
                                     target_find = re.search(r'цел\w.*($|\n)', message.text or "", flags=re.MULTILINE)
-                                    target_point = None
+                                    target_point = None # изменить, поставить None выше
                                     if target_find:
                                         pattern = re.compile(r'цел[иь]?\s([\d\s\.$-]+)', re.IGNORECASE)
                                         matches = pattern.findall(target_find[0])
@@ -63,7 +69,7 @@ while True:
                                     stop_lose_pattern = r"стоп[^\d]*лосс?[^\d]+([\d\.]+)"
                                     stop_lose_matches = re.findall(stop_lose_pattern, message.text or "", re.IGNORECASE)
                                     target_stop_lose = ", ".join(stop_lose_matches) if stop_lose_matches else None
-                                    if coin != None and input_point != None and posSide != None and target_point != None and str(input_point) != "[]" and str(target_point) != "[]":
+                                    if coin != None and input_point != None and posSide != None and target_point != None and str(input_point) != "[]" and str(target_point) != "[]": # убрать все наны, передать код что бы выходил по нану для каждого метода
                                         data = (
                                             {'message.id': message.id},
                                             {'message.sender_id': message.sender_id},
